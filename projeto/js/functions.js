31b6258575f3b2857ee928a79fb4c3c7c054c377
Danatielly <danatilly@gmail.com>
2024-11-07 16:41:14 -0300

    organizando arquivos

---
+++ projeto/js/functions.js
@@ -0,0 +1,17 @@
+function salvarOS() {
+               
+    let titulo = document.getElementById("cliente").value;
+
+    let texto = 'Teste:<br>'  +  
+    'Cliente: '       + tedocument.getElementById("cliente").value   + '<br>' +
+    'Produto: '       + document.getElementById("produto").value     + '<br>' +
+    'Descrição: '     + document.getElementById("desecricao").value  + '<br>' +
+    'Qtd. Prod.: '    + document.getElementById("qtd").value         + '<br>' +
+    'Valor Unitário: '+ document.getElementById("valorU").value      + '<br>' +
+    'Valor Total: '   + document.getElementById("valorT").value      + '<br>' +
+    'Dt. Encomenda: ' + document.getElementById("dtenc").value       + '<br>' +
+    'Dt. Entrega: '   + document.getElementById("dtEntrega").value   + '<br>' ;
+                
+
+
+        }
